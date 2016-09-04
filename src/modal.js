0c0364bf77690a64c3e15390f19767700b950244
Marcus Feitoza <sir-markus@live.com>
2016-09-04 17:35:50 -0300

    Implement Modal close.

--- src/modal.js
+++ src/modal.js
@@ -1,38 +1,52 @@
 var Modal = {
 
   oninit: function (vnode) {
-    console.log(vnode.attrs);
+    console.log("oninit");
+
+
+  },
+
+  oncreate: function (vnode) {
+    console.log("oncreate");
+  },
+
+  modalClose: function (e) {
+    m.mount(this.dom, null);
   },
 
   view: function (vnode) {
-    return [m(".slds-modal.slds-fade-in-open[role='dialog'][tabindex='-1']", [
-      m(".slds-modal__container", [
-        m(".slds-modal__header", {
-          class: vnode.attrs.header ? "" : "slds-modal__header--empty"
-        }, [
-          m("button.slds-button.slds-button--icon-inverse.slds-modal__close", [
-            m("svg.slds-button__icon.slds-button__icon--large", [
-              m("use[xlink:href='/assets/icons/action-sprite/svg/symbols.svg#close']")
+    return m(".modal-wrapper", [
+      m(".slds-modal.slds-fade-in-open[role='dialog'][tabindex='-1']", [
+        m(".slds-modal__container", [
+          m(".slds-modal__header", {
+            class: vnode.attrs.header ? "" : "slds-modal__header--empty"
+          }, [
+            m("button.slds-button.slds-button--icon-inverse.slds-modal__close", {
+              onclick: this.modalClose.bind(vnode)
+            }, [
+              m("svg.slds-button__icon.slds-button__icon--large", [
+                m("use[xlink:href='/assets/icons/action-sprite/svg/symbols.svg#close']")
+              ]),
+              m("span.slds-assistive-text", "Close")
             ]),
-            m("span.slds-assistive-text", "Close")
+            // Header
+            typeof vnode.attrs.header === "object" ? vnode.attrs.header : vnode.attrs.header
           ]),
-          // Header
-          typeof vnode.attrs.header === "object" ? vnode.attrs.header : vnode.attrs.header
+          // Content
+          m(".slds-modal__content.slds-p-around--medium", [
+            m("div", [
+              vnode.attrs.content
+            ])
+          ]),
+          // Footer
+          vnode.attrs.footer ?
+          m(".slds-modal__footer", [
+            vnode.attrs.footer
+          ]) : ""
         ]),
-        // Content
-        m(".slds-modal__content.slds-p-around--medium", [
-          m("div", [
-            vnode.attrs.content
-          ])
-        ]),
-        // Footer
-        vnode.attrs.footer ?
-        m(".slds-modal__footer", [
-          vnode.attrs.footer
-        ]) : ""
-      ]),
     ]),
-      m(".slds-backdrop.slds-backdrop--open")]
+      m(".slds-backdrop.slds-backdrop--open")
+    ])
   }
 
 };
