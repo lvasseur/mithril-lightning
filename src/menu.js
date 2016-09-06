d6722da243b51ccde484bbe497bb61e58da46257
Marcus Feitoza <sir-markus@live.com>
2016-09-05 23:04:00 -0300

    Fix menu open.

--- src/menu.js
+++ src/menu.js
@@ -1,9 +1,16 @@
 var Menu = {
 
-  isOpen: m.prop(true),
+  SIZE_CLASS: {
+    small: "slds-dropdown--small",
+    medium: "slds-dropdown--medium",
+    large: "slds-dropdown--large"
+  },
+
+  getSize: function (size) {
+    return size ? this.SIZE_CLASS[size] : "";
+  },
 
   menuSwitch: function (e) {
-    console.log("menuSwitch");
     switch (this.isOpen()) {
       case true:
         this.isOpen(false);
@@ -14,6 +21,11 @@ var Menu = {
     }
   },
 
+  oninit: function(vnode) {
+    this.isOpen = m.prop(false);
+    this.itens = vnode.attrs.itens
+  },
+
   view: function (vnode) {
     return m(".slds-dropdown-trigger.slds-dropdown-trigger--click",{
       class: this.isOpen() ? "slds-is-open" : ""
@@ -21,31 +33,22 @@ var Menu = {
       m("button.slds-button.slds-button--icon-border-filled[aria-haspopup='true']", {
         onclick: this.menuSwitch.bind(this)
       }, [
-       icon.utility("down"),
+        vnode.attrs.icon,
         m("span.slds-assistive-text", "Show More")
       ]),
-      m(".slds-dropdown.slds-dropdown--left", [
+      m(".slds-dropdown.slds-dropdown--left", {
+        class: this.getSize(vnode.attrs.size)
+      }, [
         m("ul.dropdown__list[role='menu']", [
-          m("li.slds-dropdown__item", [
-            m("a[href='javascript:void(0);'][role='menuitem']", [
-              m("p.slds-truncate", "Menu Item One")
+          this.itens.map(function (item) {
+            return m("li.slds-dropdown__item", [
+              m("a[role='menuitem']", {
+                href: item.href
+              }, [
+                m("p.slds-truncate", item.label)
+              ])
             ])
-          ]),
-          m("li.slds-dropdown__item", [
-            m("a[href='javascript:void(0);'][role='menuitem']", [
-              m("p.slds-truncate", "Menu Item Two")
-            ])
-          ]),
-          m("li.slds-dropdown__item", [
-            m("a[href='javascript:void(0);'][role='menuitem']", [
-              m("p.slds-truncate", "Menu Item Three")
-            ])
-          ]),
-          m("li.slds-dropdown__item.slds-has-divider--top-space", [
-            m("a[href='javascript:void(0);'][role='menuitem']", [
-              m("p.slds-truncate", "Menu Item Four")
-            ])
-          ])
+          })
         ])
       ])
     ])
