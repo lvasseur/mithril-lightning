1a9827150319c0aa10c1264fd35003d3cddf494c
Marcus Feitoza <sir-markus@live.com>
2016-09-05 23:44:05 -0300

    Initial implementation of form.input.

---
+++ src/form.js
@@ -0,0 +1,15 @@
+var form = {};
+
+form.input = function (attrs) {
+  return m(".slds-form-element", [
+    m("label.slds-form-element__label", {
+      for: attrs.name
+    }, [
+      attrs.input.required ? m("abbr.slds-required[title='required']", "*") : ""
+    ], attrs.label),
+    m(".slds-form-element__control", [
+      m("input.slds-input[type='text']", attrs.input)
+    ])
+  ])
+};
+
