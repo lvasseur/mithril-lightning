8bf1c04568efc243ba2d9bc4b900590864d00932
Marcus Feitoza <sir-markus@live.com>
2016-09-09 14:16:11 -0300

    Implement navigation.

---
+++ src/navigation.js
@@ -0,0 +1,36 @@
+var nav = {
+
+  oninit: function (vnode) {
+    this.active = m.prop(m.route.get());
+    this.setActive = function (e) {
+      var current = e.target.href.split("#!")[1];
+      vnode.state.active(current);
+    }
+  },
+
+  view: function (vnode) {
+    return m(".slds-grid.slds-grid--vertical.slds-navigation-list--vertical", vnode.attrs.nav, [
+      vnode.attrs.menus.map(function (menu) {
+        return [
+          m("h2.slds-text-title--caps.slds-p-around--medium", menu.label),
+          m("ul", {
+            onclick: vnode.state.setActive
+          }, [
+          menu.sub.map(function (sub) {
+            return m("li",
+              {
+                class: vnode.state.active() === sub.href ? "slds-is-active" : ""
+              }, [
+              m("a.slds-navigation-list--vertical__action.slds-text-link--reset",
+                {
+                  href: sub.href,
+                  oncreate: m.route.link
+                }, sub.label)
+            ])
+          })
+        ])]
+      })
+    ])
+  }
+
+};
