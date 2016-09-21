1cbb3fe35673ac0f53ec163eb7f81df1bc353004
Marcus Feitoza <sir-markus@live.com>
2016-09-21 09:16:38 -0300

    refactor(icons): improve icons interface

--- src/icons.js
+++ src/icons.js
@@ -10,77 +10,74 @@ var icon = {
     return size ? this.SIZE_CLASS[size] : "";
   },
 
-  standard: function (icon, attrs) {
-    var attrs = attrs || {};
+  standard: function (icon, size, attrs) {
+    var attrs = typeof size === "object" ? size : attrs || {};
+    var size = typeof size === "object" ? null : size || "";
     return m("span.slds-icon_container", {
       class: "slds-icon-standard-" + icon
     }, [
       m("svg.slds-icon[aria-hidden='true']", {
-        class: this.getSize(attrs.size)
+        class: this.getSize(size)
       }, [
         m("use[xlink:href='/assets/icons/standard-sprite/svg/symbols.svg#" + icon + "']")
-      ]),
-      // TODO: Implement assistive
-      m("span.slds-assistive-text", "Description of icon")
+      ])
     ])
   },
 
-  utility: function (icon, attrs) {
-    var attrs = attrs || {};
-    return m("span.slds-icon_container", {
-      class: "slds-icon-action-" + icon
+  utility: function (icon, size, attrs) {
+    var attrs = typeof size === "object" ? size : attrs || {};
+    var size = typeof size === "object" ? null : size || "";
+    return m("span", {
+      class: attrs.container || "slds-icon_container"
     }, [
-      m("svg.slds-icon.slds-icon-text-default[aria-hidden='true']", {
-        class: this.getSize(attrs.size) + " " + attrs.class,
-        style: attrs.style
+      m("svg", {
+        class: attrs.class ? attrs.class : "slds-icon slds-icon-text-default " + this.getSize(size || "small")
       }, [
         m("use[xlink:href='/assets/icons/utility-sprite/svg/symbols.svg#" + icon + "']")
-      ]),
-      // TODO: Implement assistive
-      m("span.slds-assistive-text", "Description of icon")
+      ])
     ])
   },
 
-  action: function (icon, attrs) {
-    var attrs = attrs || {};
-    return m("span.slds-icon_container.slds-icon_container--circle.slds-icon-action-description", {
-      class: "slds-icon-action-" + icon
+  action: function (icon, size, attrs) {
+    var attrs = typeof size === "object" ? size : attrs || {};
+    var size = typeof size === "object" ? null : size || "";
+    var cls = attrs.container || "slds-icon_container";
+    return m("span", {
+      class: cls + " " + "slds-icon_container--circle slds-icon-action-" + icon.replace("_", "-")
     }, [
       m("svg.slds-icon[aria-hidden='true']", {
-        class: this.getSize(attrs.size)
+        class: this.getSize(attrs.size || "small")
       }, [
         m("use[xlink:href='/assets/icons/action-sprite/svg/symbols.svg#" + icon + "']")
-      ]),
-      // TODO: Implement assistive
-      m("span.slds-assistive-text", "Description of icon")
+      ])
     ])
   },
 
-  doctype: function (icon, attrs) {
-    var attrs = attrs || {};
-    return m("span.slds-icon_container", [
-      m("svg.slds-icon[aria-hidden='true']", {
-        class: this.getSize(attrs.size)
-      }, [
-        m("use[xlink:href='/assets/icons/doctype-sprite/svg/symbols.svg#" + icon + "']")
-      ]),
-      // TODO: Implement assistive
-      m("span.slds-assistive-text", "Description of icon")
-    ])
-  },
-
-  custom: function (icon, attrs) {
-    var attrs = attrs || {};
-    return m("span.slds-icon_container", {
-      class: "slds-icon-custom-" + icon
+  doctype: function (icon, size, attrs) {
+    var attrs = typeof size === "object" ? size : attrs || {};
+    var size = typeof size === "object" ? null : size || "";
+    return m("span", {
+      class: attrs.container || "slds-icon_container"
     }, [
       m("svg.slds-icon[aria-hidden='true']", {
-        class: this.getSize(attrs.size)
+        class: this.getSize(size)
+      }, [
+        m("use[xlink:href='/assets/icons/doctype-sprite/svg/symbols.svg#" + icon + "']")
+      ])
+    ])
+  },
+
+  custom: function (icon, size, attrs) {
+    var attrs = typeof size === "object" ? size : attrs || {};
+    var size = typeof size === "object" ? null : size || "";
+    return m("span", {
+      class: "slds-icon_container slds-icon-custom-" + icon
+    }, [
+      m("svg.slds-icon[aria-hidden='true']", {
+        class: this.getSize(size)
       }, [
         m("use[xlink:href='/assets/icons/custom-sprite/svg/symbols.svg#" + icon + "']")
-      ]),
-      // TODO: Implement assistive
-      m("span.slds-assistive-text", "Description of icon")
+      ])
     ])
   }
 };
