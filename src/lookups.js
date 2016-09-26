1d94e3097ea1349deba5a9ce99701cf5ffb3d798
Marcus Feitoza <sir-markus@live.com>
2016-09-26 19:32:30 -0300

    refactor(lookups): some improvements
    - Improve conditional if data list is undefined
    - Add default filter function
    - Add support to error messages

--- src/lookups.js
+++ src/lookups.js
@@ -1,13 +1,13 @@
-var onKey = (function(){
+var onKey = (function () {
   var keymap = {
-    'enter' : 13,
-    'space' : 31,
-    'tab'   : 9,
-    'esc'   : 27,
-    'left'  : 37,
-    'up'    : 38,
-    'right' : 39,
-    'down'  : 40
+    'enter': 13,
+    'space': 31,
+    'tab': 9,
+    'esc': 27,
+    'left': 37,
+    'up': 38,
+    'right': 39,
+    'down': 40
   };
 
   return function bind(key, callback) {
@@ -26,15 +26,32 @@ var onKey = (function(){
   };
 }());
 
-var Lookups = {
 
+var Lookups = {
+  /**
+   * Verify if the target click is parent of lookup node
+   * if true nothing happens otherwise lookup is closed
+   *
+   * @param e
+   */
   toggle: function (e) {
-    if (!e.target.isParent(this.dom)) {
+    if (e.target.isParent(this.dom)) {
+      e.target.redraw = false;
+    } else {
       this.state.isOpen(false);
       this.state.searchTerm("");
       m.redraw()
+    }
+  },
+
+  onselect: function (item) {
+    this.state.selected(item);
+    this.state.searchTerm("");
+    this.state.isOpen(false);
+    if (typeof this.attrs.onselect === "function") {
+      this.attrs.value(this.attrs.onselect(item));
     } else {
-      e.target.redraw = false;
+      this.attrs.value(item);
     }
   },
 
@@ -44,14 +61,22 @@ var Lookups = {
 
     vnode.state.searchTerm = m.prop("");
 
+    vnode.state.handleOut = vnode.state.toggle.bind(vnode);
+
+    vnode.state.selected = m.prop(false);
+    // Default filter function
+    vnode.state.filtr = function (item) {
+      return item.name.toLowerCase().indexOf(this.searchTerm().toLowerCase()) > -1 || item.description.toLowerCase().indexOf(this.searchTerm().toLowerCase()) > -1;
+    };
+
   },
-  
+
   oncreate: function (vnode) {
-    document.body.addEventListener("click", vnode.state.toggle.bind(vnode))
+    document.body.addEventListener("click", vnode.state.handleOut)
   },
 
   onremove: function (vnode) {
-    document.body.removeEventListener("click", vnode.state.toggle.bind(vnode))
+    document.body.removeEventListener("click", vnode.state.handleOut)
   },
 
   view: function (vnode) {
@@ -64,26 +89,28 @@ var Lookups = {
         class: vnode.state.isOpen() ? "slds-is-open" : ""
       }, [
         m("label.slds-form-element__label[for='lookup-348']", vnode.attrs.label),
-        m(".slds-form-element__control", [
+        items ? m(".slds-form-element__control", [
           vnode.attrs.value() ?
-            [m(".slds-pill_container", [
-              m("span.slds-pill.slds-size--1-of-1", [
-                m("span.slds-pill__label", vnode.attrs.value().name),
-                m("button.slds-button.slds-button--icon.slds-pill__remove", {
-                  onclick: function (e) {
-                    vnode.attrs.value("");
-                    if (typeof vnode.attrs.onunselect === "function") {
-                      vnode.attrs.onunselect()
+            [
+              m(".slds-pill_container", [
+                m("span.slds-pill.slds-size--1-of-1", [
+                  m("span.slds-pill__label", vnode.state.selected()[vnode.attrs.unwrap || "name"]),
+                  m("button.slds-button.slds-button--icon.slds-pill__remove", {
+                    onclick: function (e) {
+                      vnode.attrs.value("");
+                      if (typeof vnode.attrs.onunselect === "function") {
+                        vnode.attrs.onunselect()
+                      }
                     }
-                  }
-                }, [
-                  m("svg.slds-button__icon", [
-                    m("use[xlink:href='/assets/icons/utility-sprite/svg/symbols.svg#close']")
-                  ]),
-                  m("span.slds-assistive-text", "Remove")
+                  }, [
+                    m("svg.slds-button__icon", [
+                      m("use[xlink:href='/assets/icons/utility-sprite/svg/symbols.svg#close']")
+                    ]),
+                    m("span.slds-assistive-text", "Remove")
+                  ])
                 ])
               ])
-            ])] : m(".slds-input-has-icon.slds-input-has-icon--right", [
+            ] : m(".slds-input-has-icon.slds-input-has-icon--right", [
             m("svg.slds-input__icon", [
               m("use[xlink:href='/assets/icons/utility-sprite/svg/symbols.svg#search']")
             ]),
@@ -99,55 +126,55 @@ var Lookups = {
                 vnode.state.isOpen(false);
                 e.target.blur();
               }),
-
               onfocus: function (e) {
                 vnode.state.isOpen(true);
               }
             })
           ])
-
+        ]) :
+          m(".slds-form-element__control", [
+            m(".slds-input-has-icon.slds-input-has-icon--right", [
+            m("svg.slds-input__icon", [
+              m("use[xlink:href='/assets/icons/utility-sprite/svg/symbols.svg#search']")
+            ]),
+            m("input.slds-lookup__search-input.slds-input", {
+              disabled: true,
+              placeholder: vnode.attrs.errorMsg || "Nothing found"
+            })
+          ])
         ]),
-        m(".slds-lookup__menu", [
-          m(".slds-lookup__item--label.slds-text-body--small", vnode.attrs.label),
-          m("ul.slds-lookup__list", [
-            items.filter(vnode.attrs.filtr, vnode.state).map(function (item) {
-              return m("li[role='presentation']", {
-                key: item[key],
-                onclick: function (e) {
-                  vnode.attrs.value(item);
-                  vnode.state.searchTerm("");
-                  vnode.state.isOpen(false);
-                  if (typeof vnode.attrs.onselect === "function") {
-                    vnode.attrs.onselect()
-                  }
-                }
-              }, [
-                m("span.slds-lookup__item-action.slds-media.slds-media--center", [
-                  vnode.attrs.icon,
-                  m(".slds-media__body", [
-                    m(".slds-lookup__result-text", item.name),
-                    m("span.slds-lookup__result-meta.slds-text-body--small", item.description)
+        items ? m(".slds-lookup__menu", [
+            m(".slds-lookup__item--label.slds-text-body--small", vnode.attrs.label),
+            m("ul.slds-lookup__list", [
+              items.filter(vnode.attrs.filtr || vnode.state.filtr, vnode.state).map(function (item) {
+                return m("li[role='presentation']", {
+                  key: item[key],
+                  onclick: vnode.state.onselect.bind(vnode, item)
+                }, [
+                  m("span.slds-lookup__item-action.slds-media.slds-media--center", [
+                    vnode.attrs.icon,
+                    m(".slds-media__body", [
+                      m(".slds-lookup__result-text", item.name),
+                      m("span.slds-lookup__result-meta.slds-text-body--small", item.description)
+                    ])
                   ])
                 ])
+              }),
+              m("li[role='presentation']", [
+                vnode.attrs.newButton ? m("a", {
+                  href: vnode.attrs.newButton.url,
+                  oncreate: m.route.link
+                }, [
+                  m("span.slds-lookup__item-action.slds-lookup__item-action--label]", [
+                    m("svg.slds-icon.slds-icon--x-small.slds-icon-text-default[aria-hidden='true']", [
+                      m("use[xlink:href='/assets/icons/utility-sprite/svg/symbols.svg#add']")
+                    ]),
+                    m("span.slds-truncate", vnode.attrs.newButton.label)
+                  ])
+                ]) : null
               ])
-            }),
-            m("li[role='presentation']", [
-              vnode.attrs.newButton ? m("a", {
-                href: vnode.attrs.newButton.url,
-                oncreate: m.route.link
-              }, [
-                m("span.slds-lookup__item-action.slds-lookup__item-action--label]", [
-                  m("svg.slds-icon.slds-icon--x-small.slds-icon-text-default[aria-hidden='true']", [
-                    m("use[xlink:href='/assets/icons/utility-sprite/svg/symbols.svg#add']")
-                  ]),
-                  m("span.slds-truncate", vnode.attrs.newButton.label)
-                ])
-              ]) : null
             ])
-
-
-          ])
-        ])
+          ]) : null
       ])
     ]
   }
