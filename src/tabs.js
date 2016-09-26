2272a2783c10bfe740d77c2fafe619635e904203
Marcus Feitoza <sir-markus@live.com>
2016-09-26 14:38:46 -0300

    feat(tabs): improve tabs component creation
    Improve tabs rendering
    - Remove magic index

--- src/tabs.js
+++ src/tabs.js
@@ -2,9 +2,6 @@ var tabs = {
 
   oninit: function (vnode) {
 
-    console.log("Tabs ONINIT")
-    vnode.state.items = vnode.attrs.items;
-
     vnode.state.selectedTab = m.prop(0);
 
     vnode.state.changeTab = function (i) {
@@ -15,27 +12,27 @@ var tabs = {
 
   view: function (vnode) {
 
-    var items = vnode.state.items;
+    var tabs = vnode.children;
 
     return [
       m(".slds-tabs--default", [
         m("ul.slds-tabs--default__nav[role='tablist']",
-          items.map(function (item, i) {
+          tabs.map(function (tab, i) {
             return m("li.slds-tabs--default__item.slds-text-title--caps[role='presentation'][title='Item One']",{
               class: vnode.state.selectedTab() === i ? "slds-active" : ""
             } ,[
               m("a.slds-tabs--default__link[href='#'][role='tab']", {
                 tabindex: vnode.state.selectedTab() === i ? 0 : -1,
-                id: item["name"] + "__item",
+                id: tab["name"] + "__item",
                 onclick: vnode.state.changeTab.bind(null, i)
-              } ,item["label"])
+              }, tab["label"])
             ])
           })
         ),
-        vnode.children.map(function (tabBody, i) {
+        tabs.map(function (tab, i) {
           return m(".slds-tabs--default__content[role='tabpanel']", {
             class: vnode.state.selectedTab() === i ? "slds-show" : "slds-hide"
-          }, tabBody)
+          }, tab["content"])
         })
       ])
     ]
