718d3960c0a7bbbb5deb94c8c36d1b659d3d7a49
Marcus Feitoza <sir-markus@live.com>
2016-11-08 23:36:02 -0200

    refactor(forms): input elements attrs
    Input and textarea attribute is attrs

--- src/components/forms/input.js
+++ src/components/forms/input.js
@@ -3,14 +3,17 @@
 export default {
 
   view(vnode) {
+
+    let attrs = vnode.attrs.attrs;
+
     return m(".slds-form-element", {
       className: vnode.attrs.error ? "slds-has-error" : ""
     }, [
       m("label.slds-form-element__label", {
         "for": vnode.attrs.name
-      }, vnode.attrs.input.required ? m("abbr.slds-required[title='required']", "*") : null, vnode.children),
+      }, attrs.required ? m("abbr.slds-required[title='required']", "*") : null, vnode.children),
       m(".slds-form-element__control", [
-        m("input.slds-input[type='text']", vnode.attrs.input)
+        m("input.slds-input[type='text']", attrs)
       ]),
       vnode.attrs.error ? m(".slds-form-element__help[id='error-message']", vnode.attrs.error) : null
     ])
--- src/components/forms/textarea.js
+++ src/components/forms/textarea.js
@@ -2,12 +2,15 @@
 export default {
 
   view(vnode) {
+
+    let attrs = vnode.attrs.attrs;
+
     return m(".slds-form-element", {
       className: vnode.attrs.error ? "slds-has-error" : ""
     }, [
-      m("label.slds-form-element__label[for='textarea-input-01']", vnode.attrs.textarea.required ? m("abbr.slds-required[title='required']", "*") : null,vnode.children),
+      m("label.slds-form-element__label[for='textarea-input-01']", attrs.required ? m("abbr.slds-required[title='required']", "*") : null,vnode.children),
       m(".slds-form-element__control", [
-        m("textarea.slds-textarea", vnode.attrs.textarea)
+        m("textarea.slds-textarea", attrs)
       ]),
       vnode.attrs.error ? m(".slds-form-element__help[id='error-message']", vnode.attrs.error) : null
     ])
